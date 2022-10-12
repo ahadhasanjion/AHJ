2db48874fd1c610a2ca4f1b387456b6a97e29085
ahadhasanjion <niallhoran3176@gmail.com>
2022-10-12 11:15:23 +0600

    done

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,7 +2,9 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+    <div className="m-auto text-center px-60 mt-16 mb-16">
+        <h1 className="text-3xl font-bold">Practice makes you good at programming</h1>
+        <p className="text-2xl font-normal pt-3">Check out our online computer programming quizzes to enhance your knowledge, learn new things or prepare for an upcoming test. Made up of well-researched and interesting quiz questions, each and every quiz here can test your awareness and grasp of the subject.</p>
     
     </div>
   );
